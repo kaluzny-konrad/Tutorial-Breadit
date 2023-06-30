f0a1b9d7cab4539453d92d6f2e28358ee4ce092f
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-06-30 12:52:10 +0200

    auth completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,11 @@ import Link from "next/link";
 
 import { Icons } from "./Icons";
 import { buttonVariants } from "./ui/Button";
+import { getAuthSession } from "@/lib/auth";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const session = await getAuthSession();
+
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
@@ -18,9 +21,13 @@ export default function Navbar() {
         {/* Search */}
 
         {/* Auth */}
-        <Link href="/sign-in" className={buttonVariants()}>
-          Sign In
-        </Link>
+        {session ? (
+          <p>Hi, {session.user.email}</p>
+        ) : (
+          <Link href="/sign-in" className={buttonVariants()}>
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
   );
